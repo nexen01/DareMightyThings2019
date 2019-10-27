95e6d017929a4da31dee8d2ecfe49e743ce321fc
Tommy Vadakumchery <t.vadakumchery1@gmail.com>
2019-10-27 01:58:50 -0500

    all these changes

--- server.py
+++ server.py
@@ -2,6 +2,17 @@ from flask import Flask, escape, request, render_template, url_for
 import json
 import random
 from urllib import parse
+import pandas as pd
+from subprocess import Popen, PIPE, STDOUT
+
+print('Loading data...')
+
+property_data = pd.read_pickle("prop_data.pkl")
+availability_data = pd.read_pickle("avail_data.pkl")
+place_prop_id = pd.read_pickle("place_data.pkl")
+
+prop_labels = '\t'.join(property_data.columns)
+avail_labels = '\t'.join(availability_data.columns)
 
 app = Flask(__name__)
 
@@ -10,26 +21,36 @@ with open('auth.json') as json_file:
     data = json.load(json_file)
     API_KEY = data['key']
 
-print(API_KEY)
-
-MAPS_SRC_URL_BASE = "https://www.google.com/maps/embed/v1/place?key="+API_KEY+"&q="
+print("Loaded data!")
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', api_key=API_KEY)
 
-narratives = [
-    'this place sucks',
-    'dont buy this',
-    'too bougie for you',
-    'perfect property!!!!'
-]
+def get_narrative(prop_id):
+    prop_result = property_data[property_data.PropertyID == prop_id].iloc[0]
+    prop_str = '\t'.join(str(val) for val in prop_result)
+    avail_result = availability_data[availability_data.PropertyID == prop_id].iloc[0]
+    avail_str = '\t'.join(str(val) for val in avail_result)
+    p = Popen(['java', '-jar', 'NarrativeGenerator.jar', prop_labels, prop_str, avail_labels, avail_str], stdout=PIPE, stderr=STDOUT)
+    output = next(p.stdout).decode('ascii')
+    return output
 
 @app.route('/narrative')
 def narrative():
-    address = escape(request.args.get('address'))
+    place_id = escape(request.args.get('place_id'))
+    print(place_id)
+    prop_ids = list(place_prop_id[place_prop_id.PlaceID == place_id]["PropertyID"])
+    if len(prop_ids) > 0:
+        prop_id = str(prop_ids[0])
+        if prop_id in list(availability_data["PropertyID"]):
+            return json.dumps({
+                'narrative': get_narrative(prop_id),
+                'prop_id': prop_id
+            })
+        return json.dumps({
+            'error': 'No availability entry found for the property associated with this address.'
+        })
     return json.dumps({
-        'address': address,
-        'narrative': random.choice(narratives),
-        'maps_src_url': MAPS_SRC_URL_BASE + parse.quote(address)
+        'error': 'No properties associated with this address.'
     })
--- static/index.js
+++ static/index.js
@@ -1,19 +1,94 @@
-function handleSubmit() {
-    const address = $('#usr12').val();
-    console.log(`Querying with address ${address}...`);
-    const url = window.location.href + 'narrative?address=' + address;
-    fetch(url).then(function(response) {
-        return response.json();
-    }).then(function(json) {
-        $('#narrative').text(json.narrative);
-        $('#gmaps-view').empty();
-        $('#gmaps-view').append($(
-            `<iframe src=${json.maps_src_url} width='600' height='450' allowfullscreen frameborder="0" style="border:0"></iframe>`
-        ));
+const FIELDS = ['place_id', 'formatted_address', 'geometry.location'];
+const TYPES = ['address'];
+
+function initMap(loc) {
+    $('#map').empty();
+    const map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 16,
+        center: loc
+    });
+    const marker = new google.maps.Marker({
+        position: loc,
+        map: map,
+        title: 'test'
     });
 }
 
+function displayPlace(place) {
+    $('#usr12').val(place.formatted_address);
+    initMap(place.geometry.location);
+
+    $('#narrative').text('Loading...');
+
+    const url = window.location.href + `narrative?place_id=${place.place_id.toString()}`;
+    fetch(url).then(function(response) {
+        return response.json();
+    }).then(function(json) {
+        console.log(json);
+        if ('narrative' in json) {
+            console.log(json.prop_id);
+            $('#narrative').text(json.narrative);
+        } else if ('error' in json) {
+            $('#narrative').text(json.error);
+        }
+    });
+}
+
+function handleOnChange() {
+    
+}
+
 $(function() {
-    console.log("test");
+    console.log("index.js is running!");
+
+
+
+    /**
+    const autocompleteService = new google.maps.places.AutocompleteService();
+    const placesService = new google.maps.places.PlacesService(document.getElementById('placesAttribution'));
+
+    const autocomplete = new google.maps.places.Autocomplete(
+        document.getElementById('usr12'),
+        {
+            fields: FIELDS,
+            types: TYPES
+        }
+    );
+
+    autocomplete.addListener('place_changed', function() {
+        const place = autocomplete.getPlace();
+        console.log(place);
+    
+        if ('formatted_address' in place) { // if the user selected a complete query
+            displayPlace(place);
+        } else { // if the user pressed enter prematurely
+            autocompleteService.getPlacePredictions(
+                {
+                    input: place.name,
+                    offset: place.name.length,
+                    types: TYPES
+                },
+                function(predictions, status) {
+                    if (predictions && predictions.length > 0) {
+                        const prediction = predictions[0];
+                        placesService.getDetails(
+                            {
+                                placeId: prediction.place_id,
+                                fields: FIELDS
+                            },
+                            function(predicted_place, status) {
+                                console.log(predicted_place);
+                                displayPlace(predicted_place);
+                            }
+                        )
+                    } else {
+                        $('#map').empty();
+                        $('#narrative').text('No results found.');
+                    }
+                }
+            )
+        }
+    });
+    */
 });
 
